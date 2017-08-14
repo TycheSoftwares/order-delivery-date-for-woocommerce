d01c3eae8deebe661948b28c5ed08cae6305808a
Moxa <moxa@moxa>
2017-08-14 14:56:53 +0530

    unistall.php file added
    unistall.php file is added instead of using register_deactivation_hook

--- order-delivery-date-for-woocommerce/order_delivery_date.php
+++ order-delivery-date-for-woocommerce/order_delivery_date.php
@@ -25,7 +25,6 @@ if ( !class_exists( 'order_delivery_date_lite' ) ) {
         public function __construct() {
             //Initialize settings
             register_activation_hook( __FILE__,  array( &$this, 'orddd_lite_activate' ) );
-            register_uninstall_hook( __FILE__,   array( 'order_delivery_date_lite', 'orddd_lite_deactivate' ) );
 
             add_action( 'init',                  array( &$this, 'orddd_lite_update_po_file' ) );
             add_action( 'admin_init',            array( &$this, 'orddd_lite_update_db_check' ) );
@@ -133,41 +132,6 @@ if ( !class_exists( 'order_delivery_date_lite' ) ) {
             if( !get_option( 'orddd_lite_activate_time' ) ) {
                 add_option( 'orddd_lite_activate_time', current_time( 'timestamp' ) );
             }
-        }
-
-        public static function orddd_lite_deactivate() {
-            global $orddd_lite_weekdays;
-            delete_option( 'orddd_lite_db_version' );
-            foreach ( $orddd_lite_weekdays as $n => $day_name ) {
-                delete_option( $n );
-            }
-
-            delete_option( 'orddd_lite_enable_delivery_date' );
-            delete_option( 'orddd_lite_minimumOrderDays' );
-            delete_option( 'orddd_lite_number_of_dates' );
-            delete_option( 'orddd_lite_date_field_mandatory' );            
-            delete_option( 'orddd_lite_lockout_date_after_orders' );
-            delete_option( 'orddd_lite_lockout_days' );
-            delete_option( 'orddd_lite_update_value' );
-            delete_option( 'orddd_lite_abp_hrs' );
-            delete_option( 'orddd_lite_enable_default_sorting_of_column' );
-
-            // appearance options
-            delete_option( 'orddd_lite_delivery_date_field_label' );
-            delete_option( 'orddd_lite_delivery_date_field_placeholder' );
-            delete_option( 'orddd_lite_delivery_date_field_note' );
-            delete_option( 'orddd_lite_delivery_date_format' );
-            delete_option( 'orddd_lite_number_of_months' );
-            delete_option( 'orddd_lite_calendar_theme' );
-            delete_option( 'orddd_lite_calendar_theme_name' );
-            delete_option( 'orddd_lite_language_selected' );
-            delete_option( 'orddd_lite_delivery_date_fields_on_checkout_page' );
-            delete_option( 'orddd_lite_default_appearance_settings' );    
-            delete_option( 'orddd_lite_no_fields_for_virtual_product' );
-            delete_option( 'orddd_lite_no_fields_for_featured_product' );
-
-            //holidays
-            delete_option( 'orddd_lite_holidays' );
         }
 
         // For language translation
