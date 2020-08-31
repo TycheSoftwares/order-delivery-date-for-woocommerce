20906cdd97b4455c2fb6869930079d23db311882
Rashmi Malpande <rashmimalpande@gmail.com>
2020-08-31 11:00:00 +0530

    Fix #241: fix the error coming in WP 5.5

--- order-delivery-date-for-woocommerce/js/dismiss-notice.js
+++ order-delivery-date-for-woocommerce/js/dismiss-notice.js
@@ -11,7 +11,7 @@ jQuery( document ).ready(
 				function() {
 					var $this = jQuery( this ),
 					$button   = jQuery( '<button type="button" class="notice-dismiss"><span class="screen-reader-text"></span></button>' ),
-					btnText   = commonL10n.dismiss || '';
+					btnText   = wp.i18n.dismiss || '';
 
 					// Ensure plain text
 					$button.find( '.screen-reader-text' ).text( btnText );
