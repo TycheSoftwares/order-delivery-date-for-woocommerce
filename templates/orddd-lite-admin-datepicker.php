ef1c59e6b4c6eb5e65c33fd4080d00a4ce1ec76a
Nitin <nitin@tychesoftwares.com>
2021-09-20 17:22:30 +0530

    Fixed the issue of not translating string in Edit order page
    In this commit, I have fixed the issue, Fix #357

--- templates/orddd-lite-admin-datepicker.php
+++ templates/orddd-lite-admin-datepicker.php
@@ -8,7 +8,7 @@
 ?>
 <table id="admin_delivery_fields" >
 	<tr id="admin_delivery_date_field" >
-		<td><label class ="orddd_lite_delivery_date_field_label"><?php echo esc_attr( $date_field_label ); ?>: </label></td>
+		<td><label class ="orddd_lite_delivery_date_field_label"><?php echo esc_attr__( $date_field_label , 'order-delivery-date' ); //phpcs:ignore ?>: </label></td>
 		<td>
 			<input type="text" id="<?php echo esc_attr( $field_name ); ?>" name="<?php echo esc_attr( $field_name ); ?>" class="<?php echo esc_attr( $field_name ); ?>" readonly/>
 		</td>
@@ -17,7 +17,7 @@
 	<?php if ( $time_slot_enabled ) { ?>
 
 		<tr id="admin_time_slot_field">
-			<td><label for="orddd_lite_time_slot" class=""><?php echo esc_attr( $time_field_label ); ?>: </label></td>
+			<td><label for="orddd_lite_time_slot" class=""><?php echo esc_attr__( $time_field_label , 'order-delivery-date' ); //phpcs:ignore ?>: </label></td>
 			<td><select name="orddd_lite_time_slot" id="orddd_lite_time_slot" class="orddd_lite_admin_time_slot" disabled="disabled" placeholder="">
 					<option value="select"><?php echo esc_attr__( 'Select a time slot', 'order-delivery-date' ); ?></option>
 				</select>
@@ -27,18 +27,18 @@
 	<?php } ?>
 
 	<tr id='delivery_charges'>
-		<td><label for='del_charges'><?php echo esc_attr( $fee_name ); ?></label></td>
+		<td><label for='del_charges'><?php echo esc_attr__( $fee_name , 'order-delivery-date' ); //phpcs:ignore ?></label></td>
 		<td><input type='number' min='0' value='<?php echo esc_attr( $fee ); ?>' step='0.001' id='del_charges' /></td>
 	</tr>
 
 	<tr id="delivery_charges_notes">
 		<td colspan='2'>
 			<small>
-				<?php echo esc_attr__( 'Any change in Delivery charges here will not change the order total. You will need to update    the Item section above for delivery charges to reflect in order total.', 'order-delivery-date' ); ?>
+				<?php echo esc_attr__( 'Any change in Delivery charges here will not change the order total. You will need to update the Item section above for delivery charges to reflect in order total.', 'order-delivery-date' ); ?>
 			</small>
 			<br>
 			<small>
-				<em><?php echo esc_attr__( 'Note: If you are creating the order manually, you can update the delivery date &    time after creating the order.', 'order-delivery-date' ); ?></em>
+				<em><?php echo esc_attr__( 'Note: If you are creating the order manually, you can update the delivery date & time after creating the order.', 'order-delivery-date' ); ?></em>
 			</small>
 		</td>
 	</tr>
