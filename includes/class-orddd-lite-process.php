91a2ee41c081e590cd142eade52dc86b2851c5b8
pradeeprk <pradeeprk83@gmail.com>
2022-04-19 17:00:03 +0530

    Fix #418
    [do-not-scan]

--- includes/class-orddd-lite-process.php
+++ includes/class-orddd-lite-process.php
@@ -607,7 +607,7 @@ class Orddd_Lite_Process {
 		$delivery_date = '';
 		$time_slot     = '';
 		$total_fees    = 0;
-
+		
 		if ( isset( $_POST['post_data'] ) ) { // phpcs:ignore
 
 			$delivery_date_type = preg_match( '/h_deliverydate=(.*?)&/', $_POST['post_data'], $matches ); // phpcs:ignore
@@ -624,8 +624,8 @@ class Orddd_Lite_Process {
 
 		$delivery_on_cart = get_option( 'orddd_delivery_date_on_cart_page' );
 		$is_cart          = is_cart();
-		$is_ajax          = is_ajax();
-
+		$is_ajax          = wp_doing_ajax();
+		
 		if ( '' === $delivery_date ) {
 			if ( isset( $_POST['h_deliverydate'] ) ) { // phpcs:ignore
 				 $delivery_date = $_POST['h_deliverydate']; // phpcs:ignore
