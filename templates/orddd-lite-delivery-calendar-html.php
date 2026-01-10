f28f5626c0a60ce4c196cb8ad021a7cc5e00a303
Nidhi <nidhi+66@tychesoftwares.com>
2026-01-10 12:19:00 +0530

    Enhance Filters Layout & Add CSV/Print Icons
    Fix filters layout and add icons to CSV & Print buttons.
    
    Fix #658
    
    Screenshorts :
    https://prnt.sc/YkIHSesj9SFw

--- templates/orddd-lite-delivery-calendar-html.php
+++ templates/orddd-lite-delivery-calendar-html.php
@@ -6,86 +6,90 @@
 	<div class="container-fluid pl-info-wrap">
 		<div class="row">
 			<h2><?php _e( 'Delivery Calendar', 'order-delivery-date' ); ?></h2>
-			<table class="orddd_delivery_table">
-				<tr>
-					<td>
-							<label class="orddd_label" for="orddd_filter_delivery_calendar"><?php esc_html_e('Select a view:', 'order-delivery-date'); ?></label>
-							<select class="orddd_filter_delivery_calendar" id="orddd_filter_delivery_calendar">
-								<optgroup label="<?php _e( 'Filter Deliveries by', 'order-delivery-date' ); ?>">
-									<option name="product" value="product"><?php _e( 'Products', 'order-delivery-date' ); ?></option>
-									<option name="order" value="order"><?php _e( 'Orders', 'order-delivery-date' ); ?></option>
-								</optgroup>
-							</select>
-							<input type="hidden" id="prev_event_type" value="product"/>
-						</td>
-						<td>
+
+			<div class="orddd-filters-card">
+
+				<div class="orddd-filters-row">
+
+					<!-- View -->
+					<div class="orddd-filter-field">
+						<label class="orddd_label" for="orddd_filter_delivery_calendar">
+							<?php esc_html_e('Select a view:', 'order-delivery-date'); ?>
+						</label>
+						<select class="orddd_filter_delivery_calendar" id="orddd_filter_delivery_calendar">
+							<optgroup label="<?php _e( 'Filter Deliveries by', 'order-delivery-date' ); ?>">
+								<option value="product"><?php _e( 'Products', 'order-delivery-date' ); ?></option>
+								<option value="order"><?php _e( 'Orders', 'order-delivery-date' ); ?></option>
+							</optgroup>
+						</select>
+						<input type="hidden" id="prev_event_type" value="product"/>
+					</div>
+
+					<!-- Status -->
+					<div class="orddd-filter-field">
+						<label class="orddd_label" for="orddd_filter_by_order_status">
+							<?php esc_html_e('Select status:', 'order-delivery-date'); ?>
+						</label>
+						<select class="orddd_filter_by_order_status" id="orddd_filter_by_order_status" multiple="multiple">
 							<?php
 							$order_status         = wc_get_order_statuses();
 							$default_order_status = '';
-							?>
-							<label class="orddd_label" for="orddd_filter_by_order_status"><?php esc_html_e('Select status:', 'order-delivery-date'); ?></label>
-							<select class="orddd_filter_by_order_status" id="orddd_filter_by_order_status" multiple="multiple">
-								<?php
-								foreach ( $order_status as $order_status_key => $order_status_name ) {
-									if ( $order_status_key == 'wc-pending' || $order_status_key == 'wc-processing' || $order_status_key == 'wc-on-hold' || $order_status_key == 'wc-completed' ) {
-										?>
-										<option name="<?php echo $order_status_name; ?>" value="<?php echo $order_status_key; ?>" selected><?php echo $order_status_name; ?></option>
-										<?php
-										$default_order_status .= $order_status_key . ',';
-									} elseif ( $order_status_key != 'wc-cancelled' && $order_status_key != 'wc-refunded' && $order_status_key != 'wc-failed' ) {
-										?>
-										<option name="<?php echo $order_status_name; ?>" value="<?php echo $order_status_key; ?>" selected><?php echo $order_status_name; ?></option>
-										<?php
-										$default_order_status .= $order_status_key . ',';
-									}
-								}
-								?>
-							</select>
-							<?php $default_order_status = substr( $default_order_status, 0, strlen( $default_order_status ) - 1 ); ?>
-							<input type="hidden" id="prev_order_status" value="<?php echo $default_order_status; ?>"/>
-						</td>
-						<?php if ( class_exists( 'WC_Shipping_Zones' ) ) { ?>
-						<td>
-							<?php
-							if ( class_exists( 'WC_Shipping' ) ) {
-								$shipping_methods = Orddd_Lite_Common::get_all_wc_shipping_methods();
-									?>
-									<label class="orddd_label" for="orddd_filter_by_order_shipping">
-										<?php esc_html_e( 'Select shipping method:', 'order-delivery-date' ); ?>
-									</label>
 
-									<select class="orddd_filter_by_order_shipping" id="orddd_filter_by_order_shipping" multiple="multiple">
-										<?php
-										foreach ( $shipping_methods as $method_key => $method ) {
-											?>
-											<option value="<?php echo esc_attr( $method['method_key'] ); ?>">
-												<?php echo esc_html( $method['title'] ); ?>
-											</option>
-											<?php
-										}
-										?>
-									</select>
-								<?php
+							foreach ( $order_status as $key => $label ) {
+								if ( ! in_array( $key, array( 'wc-cancelled', 'wc-refunded', 'wc-failed' ), true ) ) {
+									echo '<option value="' . esc_attr( $key ) . '" selected>' . esc_html( $label ) . '</option>';
+									$default_order_status .= $key . ',';
+								}
 							}
 							?>
-							<input type="hidden" id="prev_order_shipping" value=""/>
-						</td>
-						<?php } ?>
-						<td style="width: 100px;">
-							<label class="orddd_label">&nbsp;</label>
-							<a href="javascript:;" id="orddd_filter_calendar_data" class="button button-primary orddd_filter_btn"><?php _e( 'Apply filters', 'order-delivery-date' ); ?></a>
-						</td>
-						<td>
-							<?php do_action( 'orddd_before_export_actions' ); ?>
+						</select>
+						<input type="hidden" id="prev_order_status" value="<?php echo esc_attr( rtrim( $default_order_status, ',' ) ); ?>"/>
+					</div>
 
-							<a href="<?php echo esc_url( add_query_arg( 'download', 'orddd_data.print' ) ); ?>" target="_blank" style="float:right;margin:5px;" class="" id="orddd_print_orders"><?php _e( 'Print', 'order-delivery-date' ); ?></a>
+					<!-- Shipping -->
+					<?php if ( class_exists( 'WC_Shipping_Zones' ) ) : ?>
+					<div class="orddd-filter-field">
+						<label class="orddd_label" for="orddd_filter_by_order_shipping">
+							<?php esc_html_e( 'Select shipping method:', 'order-delivery-date' ); ?>
+						</label>
+						<select class="orddd_filter_by_order_shipping" id="orddd_filter_by_order_shipping" multiple="multiple">
+							<?php
+							$shipping_methods = Orddd_Lite_Common::get_all_wc_shipping_methods();
+							foreach ( $shipping_methods as $method ) {
+								echo '<option value="' . esc_attr( $method['method_key'] ) . '">' . esc_html( $method['title'] ) . '</option>';
+							}
+							?>
+						</select>
+						<input type="hidden" id="prev_order_shipping" value=""/>
+					</div>
+					<?php endif; ?>
 
-							<a href="<?php echo esc_url( add_query_arg( 'download', 'orddd_data.csv' ) ); ?>" style="float:right;margin:5px;" class="" id="orddd_csv_orders"><?php _e( 'CSV', 'order-delivery-date' ); ?></a>
+					<!-- Actions -->
+					<div class="orddd-filter-actions">
+						<a href="javascript:;" id="orddd_filter_calendar_data"
+						   class="button button-primary orddd_filter_btn">
+							<?php _e( 'Apply Filters', 'order-delivery-date' ); ?>
+						</a>
+					</div>
+				</div>
 
+				<div class="orddd-export-actions">
+					<a href="<?php echo esc_url( add_query_arg( 'download', 'orddd_data.csv' ) ); ?>"
+							   id="orddd_csv_orders" class="button">
+						<i class="fa-solid fa-file-csv" style="margin-right:6px;"></i>
+						<?php _e( 'CSV', 'order-delivery-date' ); ?>
+					</a>
 
-						</td>
-					</tr>
-			</table>
+					<a href="<?php echo esc_url( add_query_arg( 'download', 'orddd_data.print' ) ); ?>"
+							   target="_blank" id="orddd_print_orders" class="button">
+						<i class="fa-solid fa-print" style="margin-right:6px;"></i>
+						<?php _e( 'Print', 'order-delivery-date' ); ?>
+					</a>
+				</div>
+
+			</div>
+		</div>
+
 			<div id="orddd_events_loader">
 			<div class="orddd_events_loader_wrapper">
 				Loading delivery events...<img src=<?php echo plugins_url() . '/order-delivery-date/images/ajax-loader.gif'; ?>>
@@ -95,4 +99,3 @@
 		</br>
 	</div>
 </div>
-
